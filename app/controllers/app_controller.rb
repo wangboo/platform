302035d2b89ba4e391eb161dd89416df9da7e6c6
wangboo <advance345@126.com>
2015-05-16 17:48:53 +0800

    local cnfig

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -22,7 +22,7 @@ class AppController < AppSideController
   def server_list
     username = params[:username]
     mask = params[:mask]
-    #return resp_app_f "西楚霸王将在16:00-17:00之间维护，请小伙伴耐心等待" unless white_list.include? request.remote_ip
+    return resp_app_f "西楚霸王一测已经结束，5月17日上午10点二测华丽开启" unless white_list.include? request.remote_ip
     return resp_app_f "入参不正确" unless username and mask
     platform = Platform.where(mask: params[:mask]).first
     return resp_app_f "平台不存在" unless platform
