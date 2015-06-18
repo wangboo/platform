a953786ba96bfda51cfed53b677c3dd353cbc390
xunqin <xunqin@x.com>
2015-06-18 14:47:45 +0800

    merge

--- app/models/ios_charge_info.rb
+++ app/models/ios_charge_info.rb
@@ -26,11 +26,7 @@ class IOSChargeInfo
 		order = JiyuOrder.where(order_id: data['order_id']).first
 		# 校验订单
 		return fail_func.call "订单不存在" unless order
-<<<<<<< HEAD
 		return fail_func.call "订单中金钱和产品不对应#{data[:params]}" unless order.validate_charge data['money']
-=======
-		#return fail_func.call "订单中金钱和产品不对应" unless order.validate_charge data['money']
->>>>>>> a31f4e24e909021e752401996c4916b66d596dd1
 		# 查询该订单充值记录
 		charge_info = IOSChargeInfo.find_or_create_by(order_id: data['order_id']) do |i|
 			data.each{|k,v|i[k] = v}
