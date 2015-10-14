f88777ea7827f17d45439c7aacd421c8c6f3be49
wangkai <wangkai2027@126.com>
2015-10-14 17:55:13 +0800

    usSDK

--- app/models/u9_server.rb
+++ app/models/u9_server.rb
@@ -4,30 +4,39 @@ class U9Server
 	@default_options = {}
 	@default_options[:headers] = {"Content-Type" => "application/json"}
 	@default_options[:debug_output] = STDOUT
-	base_uri "http://sdk.g.uc.cn/cp/"
+	# base_uri "http://sdk.g.uc.cn/cp/"
+	base_uri "sdk.test4.g.uc.cn/cp/"
 
 	class << self
-	def game_id
-		"557371"
+	def game_id mask
+		if mask=="ANDOIRD-XICHU-ZHANSHEN-UC"
+			"560484"
+		else
+			"557371"
+		end
 	end
 
-	def api_key
-		"122a0218e344f51633710af7f64ccc12"
+	def api_key mask
+		if mask=="ANDOIRD-XICHU-ZHANSHEN-UC"
+			"cc0c51ccf5741d529d15291599a9f1e6"
+		else
+			"122a0218e344f51633710af7f64ccc12"
+		end
 	end
 
 	def cp_id
 		"55919"
 	end
 
-	def p uri, service, data
-		txt = data.collect{|i|i.join("=")}.join << api_key
+	def p uri, service, data,mask
+		txt = data.collect{|i|i.join("=")}.join << api_key mask
 		md5 = Digest::MD5.hexdigest txt
-		post '/account.verifySession', body: {id: Time.now.to_i, service: service,game: {gameId: game_id},
+		post '/account.verifySession', body: {id: Time.now.to_i, service: service,game: {gameId: game_id(mask)},
 			data: data, sign: md5}.to_json
 	end
 
-	def login sid
-		resp = p("/account.verifySession", "account.verifySession", {sid: sid})
+	def login sid,mask
+		resp = p("/account.verifySession", "account.verifySession", {sid: sid},mask)
 		JSON.parse(resp)
 	end
 
