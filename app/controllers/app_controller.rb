f148f37c3ad4734820a64bbe19d919aaf2735679
wangkai <wangkai2027@126.com>
2015-10-14 18:01:31 +0800

    usSDK

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -163,7 +163,7 @@ class AppController < AppSideController
     Rails.logger.debug "mask=#{params[:mask]}"
     case params[:mask]
     when /XICHU.*-UC/
-      Rails.logger.debug "uc--SDK==#{params[:mask]}"
+      Rails.logger.debug "uc--SDK==#{params[]}"
       user, account_id = android_uc params[:mask]}
     when 'IOS-ICE'
       user, account_id = ios_i4
@@ -181,10 +181,12 @@ class AppController < AppSideController
       user,account_id = ios_ky
     when /XICHU.*-BAIDU/
       user, account_id= android_baidu
-    when /ANDROID-XICHU-HUAWEI/
+    when 'ANDROID-XICHU-HUAWEI'
         user,account_id = HuaweiController.login params[:token]
-    when /ANDROID-XICHU-KUAIFA/
+    when 'ANDROID-XICHU-KUAIFA'
       user,account_id = KuaifaController.login params[:token],params[:sid]
+    when 'ANDROID-XICHU-ZHANSHEN-UC'
+      user, account_id = android_uc params[:mask]}
     else
       # 默认用sid创建一个账号
       user = QicUser.find_or_create_by(username: params[:sid]) do |u|
